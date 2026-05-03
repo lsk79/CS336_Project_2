e0887ed9a48cacd9fd2484869863104819156e35
ad1646-design <ad1646@scarletmail.rutgers.edu>
2026-05-02 22:04:25 -0400

    Update schema_subset.sql

--- schema_subset.sql
+++ schema_subset.sql
@@ -1,1 +1,12 @@
+# Angela
 
+CREATE TABLE Normalized_App(
+  id INT PRIMARY KEY,
+  as_of_year INT,
+  loan_amount_000s INT,
+  applicant_income_000s INT,
+  agency_code INT,
+  action_taken INT,
+  applicant_sex INT,
+  denial_reason_1 INT,
+);
